a5fc8c8850d184e450ad75a6a4a7011fe0c6baa8
Piotr <lordvarmiok@gmail.com>
2020-07-20 20:04:37 +0200

    And so enters pagination, master of pages

--- gifts/views.py
+++ gifts/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from gifts.models import Donation, Institution
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     donation_num = Donation.objects.all().count()
     organisation_num = Institution.objects.all().count()
 
-    context = {'donation_num': donation_num, 'organisation_num': organisation_num}
+    institutions_list = Institution.objects.all()
+    paginator = Paginator(institutions_list, 5)
+    page = request.GET.get('page')
+    institutions = paginator.get_page(page)
+
+    context = {
+        'donation_num': donation_num,
+        'organisation_num': organisation_num,
+        'institutions': institutions,
+        'paginator':paginator,
+    }
     return render(request, 'index.html', context)
 
 
